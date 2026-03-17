34aea84b6d379d36c22653254e083f8b99c81c76
likithsmart123 <likithsmart197@gmail.com>
2026-03-17 10:04:07 +0530

    Removed backend and unnecessary scripts (backend hosted separately)

--- diag_image.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'C:/Users/hp/AndroidStudioProjects/ProBuilder/app/src/main/res/drawable/img_bricks.png';
-try {
-    const buffer = fs.readFileSync(file);
-    console.log(`File size: ${buffer.length}`);
-    console.log(`First 16 bytes: ${buffer.slice(0, 16).toString('hex')}`);
-} catch (err) {
-    console.error(err);
-}
